baa4603ca3117e692a73365579e3c53ba53cb34c
Heggy Castaneda <heggyy@gmail.com>
2021-07-30 07:02:42 -0400

    add name change

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [name, setName] = useState("");
+  const [posts, setPosts] = useState([]); // https://jsonplaceholder.typicode.com/posts
+
+  useEffect(() => {
+    // when the component mounts, fetch the posts (state changes) useEffect gets called
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((data) => {
+        setPosts(data); // store resulting data from api call in state
+      });
+  }, []);
 
   const handleIncrement = () => {
+    // setCounter and pass new state
     setCounter(counter + 1);
-  }
+  };
+
+  const handleNameChange = () => {
+    setName("Heggy");
+  };
+  const postItems = posts.map((post) => (
+    <li key={post.id}>
+      {post.title}
+      <p>{post.body}</p>
+    </li>
+  ));
 
   return (
     <div>
-      <h1>{counter}</h1>
+      <h1>{counter}</h1> - <h1>{name}</h1>
       <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleNameChange}>Change Name</button>
+      <hr />
+      <ul>{postItems}</ul>
     </div>
   );
 }
